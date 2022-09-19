bdbfd2e7918eab5161106d7edd3d4ecda76ad3e1
Adigun Idowu Michael <idowumichael2509@gmail.com>
2022-09-19 23:01:52 +0100

    little fix

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
 import { Fragment, useRef, useState } from "react";
+import NavigationPrompt from "react-router-navigation-prompt";
 // import NavigationPrompt from "react-router-navigation-prompt";
 
 import Card from "../UI/Card";
@@ -16,8 +17,6 @@ const QuoteForm = (props) => {
     const enteredAuthor = authorInputRef.current.value;
     const enteredText = textInputRef.current.value;
 
-    // optional: Could validate here
-
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
@@ -27,18 +26,17 @@ const QuoteForm = (props) => {
   };
 
   const finishedHandler = () => {
-    console.log("happy?");
     setIsEntering(false);
   };
 
   return (
     <Fragment>
-      {/* <NavigationPrompt
+      <NavigationPrompt
         when={isEntering}
         message={() => {
           <p>are you sure motherfucjer</p>;
         }}
-      /> */}
+      />
       <Card>
         <form
           onFocus={focusHandler}
